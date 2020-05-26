3912d84b72b9dd685aa8777c33294226747833da
tanakakjs <oneok0813@gmail.com>
2020-05-26 11:56:54 +0900

    add vuex, vue-router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import router from "./router";
+import store from "./store";
+import App from "./App.vue";
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#root');
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#root");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view />
+  <div>Home</div>
 </template>
 
 <script>
